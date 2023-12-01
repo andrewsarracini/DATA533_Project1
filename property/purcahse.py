2e190266d245d1493b774dcee9b9937557219c31
Andrew Sarracini <andrewsarracini1@gmail.com>
2023-12-01 04:04:46 -0800

    Made mortgage calc and dislay class agnostic in purchase

--- property/purcahse.py
+++ property/purcahse.py
@@ -1,4 +1,4 @@
-import property
+from property import Property
 import random
 
 
@@ -8,15 +8,6 @@ class Purchase(Property):
         self.price = price  # subclass-specific random generation instead of here
         self.mortgage_term = mortgage_term
         self.interest = interest
-
-
-class Condo(Purchase):
-    def __init__(self):
-        sqft = random.randint(350, 800)
-        num_beds = 1
-        num_baths = 1
-        price = random.randint(300000, 500000)
-        super().__init__(sqft, num_beds, num_baths, price)
 
     def calculate_mortgage(self, downpay=0.10):
         # 10% Downpayment is a PLACEHOLDER for the variable amount
@@ -31,53 +22,69 @@ class Condo(Purchase):
                                         ** total_payments) / ((1 + monthly_interest_rate) ** total_payments - 1)
         return round(monthly_mortgage, 2)
 
-    def display_condo(self):
-        print(f'The price of this condo is: ${self.price}.00')
-        print(f'The mortgage on this condo is ${self.mortgage}.00 per month')
-        print(f'The square footage of this condo is: {self.sqft} ft.')
-        print(f'This condo has {self.num_beds} bedrooms')
-        print(f'This condo has {self.num_baths} bathrooms')
-        print(f'Condo interest rate: {self.interest}')
+    def display(self):
+        class_name = type(self).__name__
+        print(f'Purchase type: {class_name}')
+        monthly_mortgage = self.calculate_mortgage()
+        print(f'Price: ${self.price}')
+        print(f'Monthly mortgage: ${monthly_mortgage}')
+        print(f'Square footage: {self.sqft}sqft.')
+        print(f'Number of bedrooms: {self.num_beds}')
+        print(f'Number of bathrooms: {self.num_baths}')
+
+
+class Condo(Purchase):
+    def __init__(self):
+        sqft = random.randint(350, 800)
+        num_beds = random.randint(1, 2)
+        num_baths = 1
+        price = random.randint(300000, 500000)
+        super().__init__(sqft, num_beds, num_baths, price)
 
 
 class TownHome(Purchase):
     def __init__(self):
-        sqft = random.randint(800, 1800)  # Adjust the range as appropriate
+        sqft = random.randint(800, 1800)
         num_beds = random.randint(2, 4)
         num_baths = random.randint(1, 3)
         price = random.randint(550000, 700000)
-        mortgage = random.randint(1000, 7000)
-        super().__init__(sqft, num_beds, num_baths, price, mortgage)
+        super().__init__(sqft, num_beds, num_baths, price)
 
 
 class Duplex(Purchase):
     def __init__(self):
-        sqft = random.randint(1000, 2000)
+        sqft = random.randint(1000, 2100)
         num_beds = random.randint(2, 5)
         num_baths = random.randint(2, 4)
-        price = random.randint(250000, 450000)
-        mortgage = random.randint(1200, 8000)
-        super().__init__(sqft, num_beds, num_baths, price, mortgage)
+        price = random.randint(550000, 850000)
+        super().__init__(sqft, num_beds, num_baths, price)
 
 
 class Bungalow(Purchase):
     def __init__(self):
-        sqft = random.randint(900, 1500)
-        num_beds = random.randint(2, 3)
-        num_baths = random.randint(1, 2)
-        price = random.randint(150000, 350000)
-        mortgage = random.randint(900, 5000)
-        super().__init__(sqft, num_beds, num_baths, price, mortgage)
+        sqft = random.randint(1100, 2300)
+        num_beds = random.randint(4, 5)
+        num_baths = random.randint(3, 4)
+        price = random.randint(700000, 1000000)
+        super().__init__(sqft, num_beds, num_baths, price)
+
+
+class TwoStory(Purchase):
+    def __init__(self):
+        sqft = random.randint(1800, 3300)
+        num_beds = random.randint(4, 7)
+        num_baths = random.randint(3, 6)
+        price = random.randint(1000000, 2000000)
+        super().__init__(sqft, num_beds, num_baths, price)
 
 
 class Mansion(Purchase):
     def __init__(self):
-        sqft = random.randint(5000, 20000)
-        num_beds = random.randint(5, 10)
-        num_baths = random.randint(4, 8)
+        sqft = random.randint(5000, 12000)
+        num_beds = random.randint(8, 10)
+        num_baths = random.randint(7, 8)
         price = random.randint(1000000, 5000000)
-        mortgage = random.randint(5000, 20000)
-        super().__init__(sqft, num_beds, num_baths, price, mortgage)
+        super().__init__(sqft, num_beds, num_baths, price)
 
 
 prop_list = []
@@ -85,5 +92,6 @@ prop_list = []
 
 def gen_props(n, prop_type):
     for i in range(n):
-        prop_list.append(prop_type)
-        print(f'Property Type: {prop_type} {i} created')
+        prop_instance = prop_type()
+        prop_list.append(prop_instance)
+        print(f'Property Type: {prop_type.__name__} {i} created')
