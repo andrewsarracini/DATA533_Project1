ac5f1643329f9e5bb11c91665d4b67fa4f224b85
CraigAdlam <CraigAdlam>
2023-12-03 00:29:46 -0800

    Adding files to package folders

--- housemate/user/profile/userprofile.py
+++ housemate/user/profile/userprofile.py
@@ -1,6 +1,7 @@
 # userprofile.py module
 
 import os
+import re
 import pandas as pd
 from security import stringHash
 
@@ -23,20 +24,66 @@ def load_user_profiles(file_path):
         return pd.read_csv(file_path)
     return None
 
-# Function to create a new profile based on user input
+# Function to create a new profile based on user input with validation for age, email, username, and password
 def create_profile_from_input():
     profile_data = {}
     profile_data['name'] = input("Enter your name: ")
-    profile_data['age'] = int(input("Enter your age: "))
-    profile_data['email'] = input("Enter your email: ")
-    profile_data['username'] = input("Enter your username: ")
-    profile_data['password'] = input("Enter your password: ")
 
-    # Hash the username and password before storing
-    profile_data['username'] = stringHash(profile_data['username'])
-    profile_data['password'] = stringHash(profile_data['password'])
+    while True:
+        age_input = input("Enter your age: ")
+        if age_input.isdigit():
+            age = int(age_input)
+            if 11 <= age <= 999:
+                profile_data['age'] = age
+                break
+            else:
+                print("Invalid age. Please enter an integer between 11 and 999.")
+        else:
+            print("Invalid input. Please enter a valid integer for age.")
 
-    return profile_data
+    while True:
+        while True:
+            email = input("Enter your email: ")
+            # Remove leading and trailing spaces for validation
+            email = email.strip()
+
+            # Regular expression pattern for email validation
+            email_pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+            if email and re.match(email_pattern, email):
+                profile_data['email'] = email
+                break
+            else:
+                print("Invalid email format or empty input. Please enter a valid email address.")
+
+        while True:
+            username = input("Enter your username (8 characters or more): ")
+            if ' ' not in username and 8 <= len(username) <= 33:
+                profile_data['username'] = username
+                break
+            else:
+                if ' ' in username:
+                    print("Invalid username. Please avoid spaces in the username.")
+                else:
+                    print("Username should be between 8 and 33 characters.")
+
+        while True:
+            password = input("Enter your password (8 characters or more): ")
+            if ' ' not in password and 8 <= len(password) <= 133:
+                profile_data['password'] = password
+                break
+            else:
+                if ' ' in password:
+                    print("Invalid password. Please avoid spaces in the password.")
+                else:
+                    print("Password should be between 8 and 133 characters.")
+
+        # Hash the username and password before storing
+        profile_data['username'] = stringHash(profile_data['username'])
+        profile_data['password'] = stringHash(profile_data['password'])
+
+        return profile_data
+
 
 # Function to add profile information to a dataframe
 def append_to_dataframe(df, user_profile_data):
