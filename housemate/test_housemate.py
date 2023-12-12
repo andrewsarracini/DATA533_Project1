e574f065cf918543197c78031da86ba9c4722b57
CraigAdlam <cadlam87@gmail.com>
2023-12-12 00:31:54 -0800

    Adding updated test_housemate.py due to branch difficulties

--- housemate/test_housemate.py
+++ housemate/test_housemate.py
@@ -1,3 +1,4 @@
+import profile
 import unittest
 from unittest.mock import patch, MagicMock
 
@@ -13,10 +14,11 @@ from housemate import (
     profile_menu,
     housemate_menu,
     rental_user_input,
+    purchase_user_input,
     rental_main,
     purchase_main,
-    purchase_user_input,
-    rental_recommendation_main
+    rental_recommendation_main,
+    get_file_path
 )
 
 from user.security import string_hash, reverseHash, check_credentials
@@ -57,48 +59,20 @@ from property.rental import (
 )
 
 
-class TestHousemate(unittest.TestCase):
+class TestMenus(unittest.TestCase):
 
     @ classmethod
     def classSetUp(clas):
-        print('set up class')
+        pass
 
     def setUp(self):
         random.seed(123)
-
-    @patch('builtins.input', return_value='1')
-    def test_main_menu_choice1(self, mock_input):
-
-        # Redirect stdout to capture print statements
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
-
-        main_menu()
-
-        sys.stdout = sys.__stdout__
-
-        # Define expected output
-        main_menu_choice = (
+        self.main_output = (
             "Welcome to HouseMate! Please choose an option: \n"
             "1. Create Profile\n"
             "2. Login\n"
-            "3. Exit HouseMate\n"
-        )
-
-        self.assertEqual(capturedOutput.getvalue(), main_menu_choice)
-
-    @patch('builtins.input', return_value='1')
-    def test_profile_menu_choice1(self, mock_input):
-
-        # Redirect stdout to capture print statements
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
-
-        profile_menu()
-
-        # Reset stdout
-
-        profile_menu_choice = (
+            "3. Exit HouseMate\n")
+        self.profile_output = (
             "You are logged in. Please choose an option: \n"
             "1. View Profile Information\n"
             "2. Edit Profile\n"
@@ -106,44 +80,77 @@ class TestHousemate(unittest.TestCase):
             "4. View available homes\n"
             "5. Logout and return to the main menu\n"
         )
-
-        self.assertEqual(capturedOutput.getvalue(), profile_menu_choice)
-
-    @patch('builtins.input', return_value='1')
-    def test_housemate_menu_chioce1(self, mock_input):
-
-        # Redirect stdout to capture print statements
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
-
-        housemate_menu()
-
-        # Reset stdout
-
-        housemate_menu_choice = (
+        self.housemate_output = (
             "Find a home today! Please choose an option: \n"
             "1. View ONLY recommendated properties to rent\n"
             "2. View ONLY recommendated properties to purchase\n"
             "3. View available rental properties\n"
             "4. View available purchase properties\n"
             "5. Return to the profile menu\n"
-            "6. Logout and return to the main menu\n"
-        )
+            "6. Logout and return to the main menu\n")
 
-        self.assertEqual(capturedOutput.getvalue(), housemate_menu_choice)
+    def test_main_menu_choices(self):
+        choices = {
+            '1': self.main_output,
+            '2': self.main_output,
+            '3': self.main_output
+        }
 
-    @patch('builtins.input', return_value='tEsT')
-    def test_rental_user_input(self, mock_input):
-        self.assertEqual(rental_user_input(), 'test')
+        for choice, main_output in choices.items():
+            with self.subTest(choice=choice):
+                with patch('builtins.input', return_value=choice), \
+                        io.StringIO() as capturedOutput:
+                    sys.stdout = capturedOutput
 
-    @patch('builtins.input', return_value='ABCD')
-    def test_purchase_user_input(self, mock_input):
-        self.assertEqual(rental_user_input(), 'abcd')
+                    main_menu()
 
-    # @patch('builtins.input', return_value='None')
-    # def test_rental_main(self):
-    #     result = rental_main()
-    #     self.assertIsNone(result)
+                    sys.stdout = sys.__stdout__
+
+                    self.assertEqual(capturedOutput.getvalue(), main_output)
+
+    def test_profile_menu_choices(self):
+        profile_choices = {
+            '1': self.profile_output,
+            '2': self.profile_output,
+            '3': self.profile_output,
+            '4': self.profile_output,
+            '5': self.profile_output
+        }
+
+        for profile_choice, profile_output in profile_choices.items():
+            with self.subTest(profile_choice=profile_choice):
+                with patch('builtins.input', return_value=profile_choice), \
+                        io.StringIO() as capturedOutput:
+                    sys.stdout = capturedOutput
+
+                    profile_menu()
+
+                    sys.stdout = sys.__stdout__
+
+                    self.assertEqual(capturedOutput.getvalue(), profile_output)
+
+    def test_profile_menu_choices(self):
+        housemate_choices = {
+            '1': self.housemate_output,
+            '2': self.housemate_output,
+            '3': self.housemate_output,
+            '4': self.housemate_output,
+            '5': self.housemate_output,
+            '6': self.housemate_output
+        }
+
+        for housemate_choice, housemate_output in housemate_choices.items():
+            with self.subTest(housemate_choice=housemate_choice):
+                with patch('builtins.input', return_value=housemate_output), \
+                        io.StringIO() as capturedOutput:
+                    sys.stdout = capturedOutput
+
+                    housemate_menu()
+
+                    sys.stdout = sys.__stdout__
+
+                    self.assertEqual(
+                        capturedOutput.getvalue(), housemate_output)
 
     def tearDown(self):
         sys.stdout = sys.__stdout__
@@ -155,3 +162,31 @@ class TestHousemate(unittest.TestCase):
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
+
+
+class TestHelperFunctions(unittest.TestCase):
+    @ classmethod
+    def classSetUp(clas):
+        pass
+
+    def setUp(self):
+        random.seed(123)
+
+    def test_get_file_path(self):
+        self.assertEqual(get_file_path(), os.getcwd()+"\\user_profiles.csv")
+
+    @patch('builtins.input', return_value='tEsT')
+    def test_rental_user_input(self, mock_input):
+        self.assertEqual(rental_user_input(), 'test')
+
+    @patch('builtins.input', return_value='ABCD')
+    def test_purchase_user_input(self, mock_input):
+        self.assertEqual(purchase_user_input(), 'abcd')
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+
+    @classmethod
+    def tearDownClass(cls):
+        # tearDownClass is a requirement
+        pass
